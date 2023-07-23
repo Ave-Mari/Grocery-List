be1361a8a692ac9d120a1a9d4ae15eb865d8394d
Mari Vaypan <avemari98@gmail.com>
2023-07-23 23:28:23 +0300

    create input and add methods

--- src/components/AddProduct.vue
+++ src/components/AddProduct.vue
@@ -1,7 +1,26 @@
 <template>
-   
+   <section>
+      <input type="text" v-model="product" @keydown.enter="addProductToList(product)" placeholder="Type your product here">
+      <button v-if="product != ''" @click="addProductToList(product)">Add product</button>
+        <button disabled v-else>Add product</button>
+    </section>
 </template>
 
 
 <script>
+export default {
+    name: "Add Product",
+    data() {
+        return {
+            product: ''
+        } 
+    },
+    methods: {
+        addProductToList(product) {
+            this.$store.commit("addProduct", product);
+            this.product = '';
+        }
+    }
+
+  }
 </script>
