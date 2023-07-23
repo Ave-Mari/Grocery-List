ecd3cbf5c14dc49bce21d7440b10eda33fccd007
Mari Vaypan <avemari98@gmail.com>
2023-07-23 23:38:23 +0300

    create products list component

---
+++ src/components/ProductsList.vue
@@ -0,0 +1,23 @@
+<template>
+    <section>
+        <ul>
+            <li
+            v-for="product in productsList"
+            :key="product.id"
+            
+            >
+            <span :class="[product.completed ? 'completed' : 'uncompleted']">
+                {{ product.product }}
+            </span>
+            <button>
+                Done
+            </button>
+            </li>
+        </ul>
+    </section>
+
+</template>
+
+<script>
+
+</script>
